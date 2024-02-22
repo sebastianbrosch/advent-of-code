b157f6b829df31a2b1d4afa4bfa2c8341268f01a
Sebastian Brosch <code@sebastianbrosch.de>
2024-02-22 23:33:40 +0100

    solutions 2015 - 1

--- AoC2015/Day01/Solution.java
+++ AoC2015/Day01/Solution.java
@@ -7,27 +7,62 @@ public class Solution extends AoC.BaseSolution {
 
   /**
    * The main method. This method runs the solution.
+   *
    * @param args The command line arguments.
    */
   public static void main(String[] args) {
     System.out.println(Solution.class.getPackageName());
-    System.out.printf(" - Part 1: %s\n", solve1());
-    System.out.printf(" - Part 2: %s\n", solve2());
+    System.out.printf(" - Part 1: %s\n", solve1()); // solution: 138
+    System.out.printf(" - Part 2: %s\n", solve2()); // solution: 1771
   }
 
   /**
    * This function executes the first part of the puzzle.
+   *
    * @return The solution of the first part of the puzzle.
    */
   public static String solve1() {
-    return "";
+    String input = getInputString("AoC2015/Day01/input.txt");
+    Integer cntUp = 0;
+    Integer cntDown = 0;
+
+    char[] inputChars = input.toCharArray();
+
+    for (Integer i = 0; i < inputChars.length; i++) {
+      if (inputChars[i] == '(') {
+        cntUp++;
+      } else {
+        cntDown++;
+      }
+    }
+
+    return Integer.toString(cntUp - cntDown);
   }
 
   /**
    * This function executes the second part of the puzzle.
+   *
    * @return The solution of the second part of the puzzle.
    */
   public static String solve2() {
-    return "";
+    String input = getInputString("AoC2015/Day01/input.txt");
+    Integer cntUp = 0;
+    Integer cntDown = 0;
+
+    char[] inputChars = input.toCharArray();
+
+    for (Integer i = 0; i < inputChars.length; i++) {
+      if (inputChars[i] == '(') {
+        cntUp++;
+      } else {
+        cntDown++;
+      }
+
+      if (cntUp - cntDown == -1) {
+        return Integer.toString(i + 1);
+      }
+    }
+
+    return "0";
   }
 }
