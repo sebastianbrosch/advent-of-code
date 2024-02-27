80e0d0386884b7cc8f7700ac7ac959996b89208f
Sebastian Brosch <code@sebastianbrosch.de>
2024-02-27 22:59:18 +0100

    improved base solution and added function for hashing

--- AoC/BaseSolution.java
+++ AoC/BaseSolution.java
@@ -1,29 +1,20 @@
 package AoC;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 /**
  * The base solution with some useful methods.
  */
 public abstract class BaseSolution {
-
-  /**
-   * Method to get the content of a file as string.
-   *
-   * @param inputPath The file path of the file.
-   * @return The file content or an empty string if the file is not available.
-   */
-  protected static String getInputString(String inputPath) {
-    try {
-      return new String(Files.readAllBytes(Paths.get(inputPath)));
-    } catch (IOException e) {
-      e.printStackTrace();
-      return "";
-    }
-  }
+  protected final static String HASH_MD5 = "MD5";
+  protected final static String HASH_SHA1 = "SHA-1";
+  protected final static String HASH_SHA256 = "SHA-256";
 
   /**
    * Method to get the content of a file as list of strings.
@@ -37,6 +28,37 @@ public abstract class BaseSolution {
     } catch (IOException e) {
       e.printStackTrace();
       return List.of();
+    }
+  }
+
+  /**
+   * Method to get the content of a file as a string.
+   * @param inputPath The path to the file.
+   * @return The content of the file or an empty string if the file is not available.
+   */
+  protected static String getInputString(String inputPath) {
+    try {
+      return new String(Files.readString(Paths.get(inputPath)));
+    } catch (IOException e) {
+      e.printStackTrace();
+      return "";
+    }
+  }
+
+  /**
+   * Method to determine a hash from a string.
+   * @param value The string value.
+   * @param hash The hash type to be used.
+   * @return The hash of the given string or a empty string if the hash type is not known.
+   */
+  protected static String getHashOfString(String value, String hash) {
+    try {
+      MessageDigest msgDigest = MessageDigest.getInstance(hash);
+      BigInteger bigint = new BigInteger(1, msgDigest.digest(value.getBytes()));
+      String hashtext = bigint.toString(16);
+      return "0".repeat(32 - hashtext.length()) + hashtext;
+    } catch (NoSuchAlgorithmException e) {
+      return "";
     }
   }
 }
