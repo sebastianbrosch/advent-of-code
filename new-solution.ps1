638e57855eae3efbaa50d6ea0fba6184d1cf220f
Sebastian Brosch <code@sebastianbrosch.de>
2024-12-01 11:44:01 +0100

    changed scripts to support C++ and AoC 2024

--- new-solution.ps1
+++ new-solution.ps1
@@ -7,19 +7,23 @@
   The year of the solution to create.
 .PARAMETER Day
   The day of the solution to create.
+.PARAMETER Language
+  The language of the solution to create.
 .NOTES
   Author: Sebastian Brosch
 .EXAMPLE
-  Create the solution of Advent of Code 2023 - Day 1.
-  .\New-Solution -Year 2023 -Day 1
+  Create the solution of Advent of Code 2024 - Day 1 using C++.
+  .\New-Solution -Year 2024 -Day 1 -Language C++
+  .\New-Solution 2024 1 C++
 #>
 param(
   [UInt16]$Year,
-  [UInt16]$Day
+  [UInt16]$Day,
+  [String]$Language
 )
 
 $firstYear = 2015
-$lastYear = 2023
+$lastYear = 2024
 
 if ((-not $Year) -or ($Year -lt $firstYear) -or ($Year -gt $lastYear)) {
   throw "No valid year was specified. Please specify a valid year ($firstYear to $lastYear)."
@@ -29,24 +33,43 @@ if ((-not $Day) -or ($Day -lt 1) -or ($Day -gt 25)) {
   throw "No valid day was specified. Please specify a valid day (1 to 25)."
 }
 
+if (@('C++', 'JAVA') -notcontains $Language.ToUpper()) {
+  throw "No valid language was specified. Please specify a valid language (C++, Java)."
+}
+
 $formatDay = '{0:d2}' -f $Day
 $folderPathDay = "AoC$Year/Day$formatDay";
-$filePathSolution = "$folderPathDay/Solution.java"
 $urlPuzzle = "https://adventofcode.com/$Year/day/$Day"
 
 if (-not (Test-Path -PathType Container $folderPathDay)) {
   New-Item -ItemType Directory -Path $folderPathDay | Out-Null
 }
 
-if ((Test-Path -PathType Leaf "AoC/Solution.java") -and (Test-Path -PathType Container $folderPathDay)) {
-  if (-not (Test-Path -PathType Leaf "$folderPathDay/Solution.java")) {
-    Copy-Item "AoC/Solution.java" -Destination $folderPathDay
+switch ($Language.ToUpper()) {
+  'C++' {
+    if ((Test-Path -PathType Leaf "AoC/Solution.cc") -and (Test-Path -PathType Container $folderPathDay)) {
+      if (-not (Test-Path -PathType Leaf "$folderPathDay/Solution.cc")) {
+        Copy-Item "AoC/Solution.cc" -Destination $folderPathDay
+      }
+    }
+
+    $filePathSolution = "$folderPathDay/Solution.cc"
+    (Get-Content $filePathSolution).Replace('$Year', "$Year") | Set-Content $filePathSolution
+    (Get-Content $filePathSolution).Replace('$Day', "$Day") | Set-Content $filePathSolution
+  }
+  'JAVA' {
+    if ((Test-Path -PathType Leaf "AoC/Solution.java") -and (Test-Path -PathType Container $folderPathDay)) {
+      if (-not (Test-Path -PathType Leaf "$folderPathDay/Solution.java")) {
+        Copy-Item "AoC/Solution.java" -Destination $folderPathDay
+      }
+    }
+
+    $filePathSolution = "$folderPathDay/Solution.java"
+    (Get-Content $filePathSolution).Replace('package AoC;', "package AoC$year.Day$formatDay;") | Set-Content $filePathSolution
+    (Get-Content $filePathSolution).Replace('$Year', "$Year") | Set-Content $filePathSolution
+    (Get-Content $filePathSolution).Replace('$Day', "$Day") | Set-Content $filePathSolution
   }
 }
-
-(Get-Content $filePathSolution).Replace('package AoC;', "package AoC$year.Day$formatDay;") | Set-Content $filePathSolution
-(Get-Content $filePathSolution).Replace('$Year', "$Year") | Set-Content $filePathSolution
-(Get-Content $filePathSolution).Replace('$Day', "$Day") | Set-Content $filePathSolution
 
 Write-Host "Advent of Code $Year - Day ${formatDay}: $urlPuzzle"
 Write-Host "Advent of Code $Year - Day ${formatDay} - Input: $urlPuzzle/input"
--- run-solution.ps1
+++ run-solution.ps1
@@ -7,19 +7,23 @@
   The year of the solution to compile and run.
 .PARAMETER Day
   The day of the solution to compile and run.
+.PARAMETER Language
+  The language of the solution to compile and run.
 .NOTES
   Author: Sebastian Brosch
 .EXAMPLE
-  Run the solution of Advent of Code 2023 - Day 1.
-  .\Run-Solution -Year 2023 -Day 1
+  Run the solution of Advent of Code 2024 - Day 1 using C++.
+  .\Run-Solution -Year 2024 -Day 1 -Language C++
+  .\Run-Solution 2024 1 C++
 #>
 param(
   [UInt16]$Year,
-  [UInt16]$Day
+  [UInt16]$Day,
+  [String]$Language
 )
 
 $firstYear = 2015
-$lastYear = 2023
+$lastYear = 2024
 
 if ((-not $Year) -or ($Year -lt $firstYear) -or ($Year -gt $lastYear)) {
   throw "No valid year was specified. Please specify a valid year ($firstYear to $lastYear)."
@@ -29,18 +33,39 @@ if ((-not $Day) -or ($Day -lt 1) -or ($Day -gt 25)) {
   throw "No valid day was specified. Please specify a valid day (1 to 25)."
 }
 
-if ((-not (Get-Command java -ErrorAction SilentlyContinue)) -or (-not (Get-Command javac -ErrorAction SilentlyContinue))) {
-  throw "Java is not installed. Please install Java."
+if (@('C++', 'JAVA') -notcontains $Language.ToUpper()) {
+  throw "No valid language was specified. Please specify a valid language (C++, Java)."
+}
+
+switch ($Language.ToUpper()) {
+  'C++' {
+    if ((-not (Get-Command g++ -ErrorAction SilentlyContinue))) {
+      throw "C++ Compiler is not installed. Please install G++."
+    }
+  }
+  'JAVA' {
+    if ((-not (Get-Command java -ErrorAction SilentlyContinue)) -or (-not (Get-Command javac -ErrorAction SilentlyContinue))) {
+      throw "Java is not installed. Please install Java."
+    }
+  }
 }
 
 $formatDay = '{0:d2}' -f $Day
 $folderPathDay = "AoC$Year/Day$formatDay"
-$filePathSolution = "$folderPathDay/Solution.java"
 $urlPuzzle = "https://adventofcode.com/$Year/day/$Day"
 
-if (Test-Path $filePathSolution -PathType Leaf) {
-  javac $filePathSolution
-  java $filePathSolution.Replace('/', '.').Replace('.java', '')
+switch ($Language.ToUpper()) {
+  'C++' {
+    Push-Location -Path $folderPathDay
+    g++ -o Solution.exe Solution.cc
+    & ".\Solution.exe"
+    Pop-Location
+  }
+  'JAVA' {
+    $filePathSolution = "$folderPathDay/Solution.java"
+    javac $filePathSolution
+    java $filePathSolution.Replace('/', '.').Replace('.java', '')
+  }
 }
 
 Write-Host "Advent of Code $Year - Day ${formatDay}: $urlPuzzle"
