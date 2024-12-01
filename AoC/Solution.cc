8fe6bcc0c4f08b3c75f59e03e821bcd04c70ca44
Sebastian Brosch <code@sebastianbrosch.de>
2024-12-01 11:49:10 +0100

    added template for C++ solution

---
+++ AoC/Solution.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+/**
+ * This function executes the first part of the puzzle.
+ *
+ * @return The solution of the first part of the puzzle.
+ */
+std::string part1() {
+  return "";
+}
+
+/**
+ * This function executes the second part of the puzzle.
+ *
+ * @return The solution of the second part of the puzzle.
+ */
+std::string part2() {
+  return "";
+}
+
+/**
+ * A solution for Advent of Code.
+ *
+ * @see https://adventofcode.com/$Year/day/$Day
+ * @see https://adventofcode.com/$Year/day/$Day/input
+ */
+int main() {
+  std::cout << "AoC$Year.Day$Day" << std::endl;
+  std::cout << " - Part 1: " << part1() << std::endl;
+  std::cout << " - Part 2: " << part2() << std::endl;
+  return 0;
+}
